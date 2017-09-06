5dac4d91bb97ae84998d03c61c03fffdbd73a7c6
gyt95 <gyt95@163.com>
2017-09-06 18:53:30 +0800

    use socket.io

--- chat-room/wsServer.js
+++ chat-room/wsServer.js
@@ -1,50 +1,36 @@
-var ws = require("nodejs-websocket")
+// var ws = require("nodejs-websocket")
+var app = require('http').createServer()
+var io = require('socket.io')(app)
 
 var PORT = 4000
 
 //需要给每个客户端写个名字，定义计数器
 var clientCount = 0
 
-var server = ws.createServer(function (conn) {
-    
+app.listen(PORT)
+
+io.on('connection', function(socket){
     clientCount++
-    conn.nickname = 'user' + clientCount
+    socket.nickname = 'user' + clientCount
 
-    var mes = {}
-    mes.type = 'enter'
-    mes.data = conn.nickname + ' comes in '
-    //向每个客户端广播消息
-    broadcast(JSON.stringify(mes))//为什么这里不能直接发送mes，而需要JSON.stringify(mes)
+    /**
+     * 这里要注意，socket.emit()和io.emit()是有区别的
+     * 
+     * socket.emit()中的emit指发送消息。
+     * io.emit()则是广播消息
+     */
+    io.emit('enter', socket.nickname + ' comes in ')
+    
+    socket.broadcast.emit('user connected');
 
-    //有客户端发送消息时，广播此消息
-	conn.on("text", function (str) {
-        console.log("Receive msg: "+ str)
-        var mes = {}
-        mes.type = 'message'
-        mes.data = conn.nickname + '：'+str
-        broadcast(JSON.stringify(mes))
+    socket.on('message', function(str){
+        io.emit('message', socket.nickname + '：' + str)
     })
 
-    //客户端关闭时，广播此消息
-	conn.on("close", function (code, reason) {
-        console.log("Connection closed")
-        var mes = {}
-        mes.type = 'leave'
-        mes.data = conn.nickname + ' left '
-        broadcast(JSON.stringify(mes))
+    socket.on('disconnect', function(){
+        io.emit('leave', socket.nickname + ' left ')
     })
 
-    conn.on("error", function (err) {
-        console.log("handle err")
-        console.log(err)
-    })
-}).listen(PORT)
+})
 
 console.log("WebSocket server listening on port " + PORT)
-
-function broadcast(str){
-    //取所有连接，让每个连接去调用sendText()
-    server.connections.forEach(function(connection) {
-        connection.sendText(str)   
-    });
-}
