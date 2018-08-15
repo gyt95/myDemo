5bef1656665cd83265220eece9f66fdb5730b991
gyt95 <gyt95@163.com>
2018-08-15 23:57:57 +0800

    Add some comments in node-crawler/app10.js

--- node-crawler/app10.js
+++ node-crawler/app10.js
@@ -1,6 +1,6 @@
-const request = require('request')
-const cheerio = require('cheerio')
-const mysql = require('mysql')
+var request = require('request')
+var cheerio = require('cheerio')
+var mysql = require('mysql')
 // 创建数据库连接
 var db = mysql.createConnection({
     host:     'localhost',
@@ -8,9 +8,10 @@ var db = mysql.createConnection({
     password: '123456',
     database: 'spider_test'
   });
-  db.connect();  //连接
+db.connect();  //连接
 
-  function show(item){
+// 抓取电影链接
+function show(item){
     request('http://www.1905.com/vod/list/n_1_t_1/o1p'+item+'.html',function(err,res){  
         if(err){  
             console.log('请求出错');  
@@ -37,6 +38,30 @@ var db = mysql.createConnection({
             }); 
         }  
     });
-  }
+}
   
-  show(1)
+show(1)
+
+// PS1: 暂时要手动ctrl+c强制退出，否则不断爬取。
+// PS2: 仅仅用于尝试爬虫后存入数据库，还没涉及代理池等知识。
+
+/**
+ * 需要先手动创建数据库spider_test
+ * (
+ *  mysql -uroot -p123456 
+ *  show databases;
+ *  create database spider_test;
+ *  use spider_test;
+ * )
+ * 
+ * 创建数据表
+ * 
+ * create table blog(
+ * id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ * title VARCHAR(100) NOT NULL,
+ * href VARCHAR(100) NOT NULL,
+ * title2 VARCHAR(100) NOT NULL,
+ * time VARCHAR(100) NOT NULL,
+ * codeNum INT(5) NOT NULL
+ * );
+ */
